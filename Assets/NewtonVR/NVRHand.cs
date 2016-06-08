9a45eae2c2e03ab8607fc95a001af1fbc760013c
Dinos Tsiknis <dinos.tsiknis@charmgames.com>
2016-06-08 13:05:41 -0700

    Fix race condition between SetDeviceIndex and RenderModelLoaded when controllers connected after Awake has been called

--- Assets/NewtonVR/NVRHand.cs
+++ Assets/NewtonVR/NVRHand.cs
@@ -664,6 +664,12 @@ namespace NewtonVR
         {
             DeviceIndex = index;
             Controller = SteamVR_Controller.Input(index);
+
+            // Ensure the render model gets updated to avoid any race conditions
+            // between this call and the render_model_loaded event
+            SteamVR_RenderModel renderModel = this.GetComponentInChildren<SteamVR_RenderModel>();
+            UpdateRenderModelLoadState(renderModel);
+
             StartCoroutine(DoInitialize());
         }
 
@@ -764,7 +770,14 @@ namespace NewtonVR
                 RenderModel.transform.localRotation = Quaternion.identity;
             }
         }
-
+ 
+        private void UpdateRenderModelLoadState(SteamVR_RenderModel renderModel)
+        {
+            if ((int)renderModel.index == DeviceIndex) {
+                RenderModelInitialized = true;
+            }
+        }
+        
         public void Initialize()
         {
             Rigidbody = this.GetComponent<Rigidbody>();
