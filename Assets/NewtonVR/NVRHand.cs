c7efa24d768a7b84b034f3a594082dc4782389c9
Dinos Tsiknis <dinos.tsiknis@charmgames.com>
2016-06-11 19:41:07 -0700

    Restrict ghost renderers to only mesh renderer types

--- Assets/NewtonVR/NVRHand.cs
+++ Assets/NewtonVR/NVRHand.cs
@@ -822,7 +822,7 @@ namespace NewtonVR
                     Color transparentcolor = Color.white;
                     transparentcolor.a = (float)VisibilityLevel.Ghost / 100f;
 
-                GhostRenderers = this.GetComponentsInChildren<Renderer>();
+                GhostRenderers = this.GetComponentsInChildren<MeshRenderer>();
                     for (int rendererIndex = 0; rendererIndex < GhostRenderers.Length; rendererIndex++)
                     {
                         NVRHelpers.SetTransparent(GhostRenderers[rendererIndex].material, transparentcolor);
@@ -843,7 +843,7 @@ namespace NewtonVR
                     Color transparentcolor = Color.white;
                     transparentcolor.a = (float)VisibilityLevel.Ghost / 100f;
 
-                GhostRenderers = this.GetComponentsInChildren<Renderer>();
+                GhostRenderers = this.GetComponentsInChildren<MeshRenderer>();
                     for (int rendererIndex = 0; rendererIndex < GhostRenderers.Length; rendererIndex++)
                     {
                         NVRHelpers.SetTransparent(GhostRenderers[rendererIndex].material, transparentcolor);
