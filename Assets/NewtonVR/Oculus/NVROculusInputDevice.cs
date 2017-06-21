bfe25f3f026197b3f617a8318f5d9d334a3f0ce2
Dinos Tsiknis <dinos.tsiknis@charmgames.com>
2017-06-20 18:06:04 -0700

    Added ApplicationMenu button mapping to oculus input

--- Assets/NewtonVR/Oculus/NVROculusInputDevice.cs
+++ Assets/NewtonVR/Oculus/NVROculusInputDevice.cs
@@ -52,6 +52,7 @@ namespace NewtonVR
             ButtonMapping.Add(NVRButtons.Trigger, OVRInput.Button.PrimaryIndexTrigger);
             ButtonMapping.Add(NVRButtons.Grip, OVRInput.Button.PrimaryHandTrigger);
             ButtonMapping.Add(NVRButtons.System, OVRInput.Button.Back);
+            ButtonMapping.Add(NVRButtons.ApplicationMenu, OVRInput.Button.Start);
 
             TouchMapping.Add(NVRButtons.A, OVRInput.Touch.One);
             TouchMapping.Add(NVRButtons.B, OVRInput.Touch.Two);
