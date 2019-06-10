001173ba989f2d6528d6c5ed167c92070caf60ee
Shervin Mortazavi <shervin.mortazavi@charmgames.com>
2019-06-10 12:52:06 -0700

    Added OnInitialized events and helper methods, useful for callbacks when the NVRIntegration is initialized.

--- Assets/NewtonVR/NVRIntegration.cs
+++ Assets/NewtonVR/NVRIntegration.cs
@@ -10,6 +10,24 @@ namespace NewtonVR
     {
         protected NVRPlayer Player;
 
+        protected event Action OnInitialized;
+
+        public void AddOnInitializedListener(Action callback) {
+            OnInitialized += callback;
+        }
+
+        public void RemoveOnInitializedListener(Action callback) {
+            OnInitialized -= callback;
+        }
+
+        protected void InvokeOnInitializedEvent()
+        {
+            if (OnInitialized != null)
+            {
+                OnInitialized();
+            }
+        }
+
         public abstract void Initialize(NVRPlayer player);
 
         public abstract void DeInitialize();
